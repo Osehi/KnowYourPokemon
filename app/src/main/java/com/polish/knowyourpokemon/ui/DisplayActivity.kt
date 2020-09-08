93caf252042678b4eeeff99feb6e31863799dc27
Osehi <osehiaseehilen@gmail.com>
2020-09-08 06:04:00 +0100

    display activity created

---
+++ app/src/main/java/com/polish/knowyourpokemon/ui/DisplayActivity.kt
@@ -0,0 +1,12 @@
+package com.polish.knowyourpokemon.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.polish.knowyourpokemon.R
+
+class DisplayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display)
+    }
+}
